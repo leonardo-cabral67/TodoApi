14bbbe9a6bdb95a4e4ae09aa9e1bcb6dcf0b360f
Leonardo <leonardolccabral@gmail.com>
2025-05-08 20:53:37 -0300

    refactor: change `Program.cs` to use minimal api with routes class

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-using Microsoft.VisualBasic;
+using TodoApi.Data;
 using TodoApi.Models;
-using TodoApi.Repositories;
+using TodoApi.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<ITodoRepository, TodoRepository>();
+builder.Services.AddScoped<TodoContext>();
 
 var app = builder.Build();
 
@@ -17,53 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.TodoRoutes();
+
 app.UseHttpsRedirection();
-
-var todoApi = app.MapGroup("api/todo");
-
-todoApi.MapGet("/", (ITodoRepository repository, ILogger<Program> logger) =>
-{
-    logger.LogInformation("Loading all Items");
-    return Results.Ok(repository.GetAll());
-});
-
-
-todoApi.MapGet("/{id}", (int id, ITodoRepository repository, ILogger<Program> logger) =>
-{
-    logger.LogInformation($"Loading item by id: {id}");
-    var item = repository.GetById(id);
-    if (item == null)
-    {
-        logger.LogWarning($"Task with id {id} not found");
-        return Results.NotFound();
-
-    }
-    return Results.Ok(item);
-});
-
-todoApi.MapPost("/", (TodoItem item, ITodoRepository repository, ILogger<Program> logger) =>
-{
-    logger.LogInformation("Creating new task");
-    TodoItem createdItem = repository.Create(item);
-    return Results.Created($"api/todo/{createdItem.Id}", createdItem);
-});
-
-todoApi.MapPut("/{id}", (int id, TodoItem item, ITodoRepository repository, Logger<Program> logger) =>
-{
-    if (id != item.Id)
-        return Results.BadRequest("Url ID is differente from id in body");
-    logger.LogInformation($"Item with id {id} is being updated...");
-    var updateItem = repository.Update(item);
-    if (updateItem == null)
-        return Results.NotFound();
-    return Results.Created("result", updateItem);
-});
-
-todoApi.MapDelete("/{id}", (int id, ITodoRepository repository, ILogger<Program> logger) =>
-{
-    var deleteItem = repository.Delete(id);
-    if (deleteItem == null)
-        return Results.NotFound();
-    logger.LogInformation($"Deleted item with id {id}");
-    return Results.Ok();
-});
+app.Run();
